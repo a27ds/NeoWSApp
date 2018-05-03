bdb3341bb178d6628c75c7c3b56a278846e3276d
Peter Karlsson <itPet@peters-macbook-pro-3.local>
2018-05-03 13:57:20 +0200

    CustomCell is connected to TableView. TableView displays test data.

--- NeoWSApp/controller/StartViewController.swift
+++ NeoWSApp/controller/StartViewController.swift
@@ -8,15 +8,44 @@
 
 import UIKit
 
-class StartViewController: UIViewController {
+class StartViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    //MARK: - Variables
     @IBOutlet weak var startTableView: UITableView!
     
+    var listOfAsteroids = [Asteroid]()
+    
+    //MARK: - StartUp functions
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        
+        //Add test data to list
+        listOfAsteroids.append(Asteroid(name: "Asteroid nr 555", distance: 230, size: 15, isDangerous: true))
+    }
+    
+    //MARK: - TableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listOfAsteroids.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = Bundle.main.loadNibNamed("CustomCell", owner: self, options: nil)?.first as! CustomCell
+        
+        let asteroid = listOfAsteroids[indexPath.row]
+        
+        cell.nameLabel.text = "Name: \(asteroid.name)"
+        cell.distanceLabel.text = "Distance from Earth: \(asteroid.distance) km"
+        cell.dangerousLabel.text = "Asteroid is dangerous: \(asteroid.isDangerous)"
+        cell.sizeLabel.text = "Diameter: \(asteroid.size) m"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
     }
 
+    //MARK: - MemoryWarning
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
