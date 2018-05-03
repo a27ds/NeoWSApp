7a5a87e7458f74988286ecef786fff17868eabef
Niklas Lindell <niklas_lindell@hotmail.com>
2018-05-03 13:25:48 +0200

    Storryboard halvklar

--- NeoWSApp/controller/StartViewController.swift
+++ NeoWSApp/controller/StartViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 class StartViewController: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
     }
 
     override func didReceiveMemoryWarning() {
